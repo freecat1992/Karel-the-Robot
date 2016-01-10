8c7047662916fd40a4a1746eba47cba15051a562
S <s>
2016-01-10 00:21:07 -0800

    {"types":"C","runs":9}

--- CollectNewspaperKarel.java
+++ CollectNewspaperKarel.java
@@ -13,5 +13,40 @@ import stanford.karel.*;
 public class CollectNewspaperKarel extends SuperKarel {
 
 	// You fill in this part
-
+	public void run() {
+		gotoDoor();
+		pickBeeper();
+		goBack();
+		
+	}
+	/*go to the place where the beeper is */
+	private void gotoDoor(){
+		if (frontIsClear()){
+			move();
+		}else {
+			turnRight();
+			move();
+		}
+		if (leftIsClear()){
+			turnLeft();
+			move();
+		}
+	}
+	
+	private void goBack(){
+		turnAround();
+		move();
+		turnRight();
+		moveBack();
+	}
+	
+	private void moveBack(){
+		if (frontIsClear()){
+			move();
+		}else {
+			turnLeft();
+			move();
+		}
+	}
+	
 }
