d4460b109d11cd40ece3723202f3f645552227bc
S <s>
2016-01-11 22:26:47 -0800

    {"types":"C","runs":167}

--- StoneMasonKarel.java
+++ StoneMasonKarel.java
@@ -88,120 +88,14 @@ public class StoneMasonKarel extends SuperKarel {
 			 }
 		
 
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		/*
-		// Repeat the period until goal (front and left are blocked)
-		// Finish a single period:
-		// 0) Turn to north
-		turnLeft();
-			
-		// 1) Move forward until blocked, put beepers at empty slots
-		 if (noBeepersPresent()) {
-			 putBeeper();
-		 } else {
-			 move();
-		 }
-		 while (frontIsClear()) {
-			 move();
-			 if (noBeepersPresent()) {
-				 putBeeper();	 
-			 }
-	
-		 }
-	
-	
-		
-		// 2) Move 4 units East
-	    turnRight();
-	    for (int i=0; i<4; i++){
-			move();
-			}
-		
-			
-			// 3) Turn to south, repeat 1)
-	    turnRight();
-	    if (noBeepersPresent()) {
-			 putBeeper();
-		 } else {
-			 move();
-		 }
-		 while (frontIsClear()) {
-			 move();
-			 if (noBeepersPresent()) {
-				 putBeeper();	 
-			 }
-	
-		 }
-	    		
-			// 4) Move 4 units East
-		 turnLeft();
-		 for (int i=0; i<4; i++){
-				move();
-				}
-	
-	*/	 		 
-	}	    
-
 	}
-
-/*
-		turnLeft();
-		while (noBeepersPresent()){
-			putBeeper();
-			while (beepersPresent()){
-				move();
-		if (frontIsBlocked()){
-			turnRight();
-		for (int i=0; i<4; i++){
-					move();
-					}
-		if (leftIsBlocked()){
-			  turnRight();
-		  }
-			}
-		}
-	  }
-	}
-	
-  
-	private void finishSecondColumn(){
-	  
-		   while (noBeepersPresent()){
-				putBeeper();
-				while (beepersPresent()){
-					move();
-			if (frontIsBlocked()){
-				turnLeft();
-			for (int i=0; i<4; i++){
-						move();
-						}
-			if (rightIsBlocked()){
-				  turnLeft();
-			  }
-				}
-			}
-		  }
-	  
-	}
-	
-	private void finishThirdColumn(){
-	    
-		 while (noBeepersPresent()){
-				putBeeper();
-				while (beepersPresent()){
-					move();
+}
 		
-	    }
-      }
-	}
-}*/
+		
+		
+		
+		
+		
+		
+		
+	
