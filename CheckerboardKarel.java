9fb6cbe50d4f4ddf0d155286bf6ed38a56b89195
S <s>
2016-01-11 22:29:48 -0800

    {"types":"C","runs":169}

--- CheckerboardKarel.java
+++ CheckerboardKarel.java
@@ -26,7 +26,8 @@ public void run() {
 // 1) if facing east, turn left
 //    if there is beeper, move, turn left,move, put beeper, repeat 2
 //	  if no beeper, move, put beeper, turn left,repeat 2
-	if (facingEast()) {
+	while (frontIsBlocked()) {
+			if (facingEast()) {
 		turnLeft();
 		if (beepersPresent()) {
 			move();
@@ -39,6 +40,7 @@ public void run() {
 			turnLeft();
 		}
 	}
+}
 // 2) if facing west, turn right
 //    if there is beeper, move, turn right,move, put beeper, repeat 2
 //	  if no beeper, move, put beeper, turn right,repeat 2
