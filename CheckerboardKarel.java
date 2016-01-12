6d224305c7a025caece57add45cd537dad62ebed
S <s>
2016-01-11 22:35:19 -0800

    {"types":"C","runs":171}

--- CheckerboardKarel.java
+++ CheckerboardKarel.java
@@ -14,33 +14,20 @@ public class CheckerboardKarel extends SuperKarel {
 	// You fill in this part
 public void run() {
 	
-//1 put a beeper at the starting unit
-	putBeeper();
-//2  while front is clear, move, move, put beeper
-	while (frontIsClear()) {
-		move();
-		move();
+
+//1  while there is beeper, move; while no beeper, put beeper, move
+	while (noBeepersPresent()) {
 		putBeeper();
+	}
+	while (beepersPresent()) {
+		move();
 	}
 // if front is blocked, 
 // 1) if facing east, turn left
 //    if there is beeper, move, turn left,move, put beeper, repeat 2
 //	  if no beeper, move, put beeper, turn left,repeat 2
-	while (frontIsBlocked()) {
-			if (facingEast()) {
-		turnLeft();
-		if (beepersPresent()) {
-			move();
-			turnLeft();
-			move();
-			putBeeper();
-		} else {
-			move();
-			putBeeper();
-			turnLeft();
-		}
+	
 	}
-}
 // 2) if facing west, turn right
 //    if there is beeper, move, turn right,move, put beeper, repeat 2
 //	  if no beeper, move, put beeper, turn right,repeat 2
